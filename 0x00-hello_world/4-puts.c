b8ed24fe07263308311e31520a746450e1b8b5d4
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-02 05:32:22 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,16 @@
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+   * main - main block
+ * return: 0
+   */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
