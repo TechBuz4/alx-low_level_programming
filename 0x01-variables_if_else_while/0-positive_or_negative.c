ffafbca07a4e4851bb47e3945e8ce81717d2d0d6
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-03 05:23:44 -0700

    ok

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 
 {
-	printf(&n, "%d is positive\n");
+	printf("%d is positive\n", n);
 
 }
 
@@ -25,7 +25,7 @@ else if (n < 0)
 
 {
 
-	printf("&n,  %d is negative\n");
+	printf("%d is negative\n", n);
 
 }
 
