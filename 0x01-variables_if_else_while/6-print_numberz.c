1ec8f51781077be8c0d05648fd664ad4bb35dc86
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-03 08:09:08 -0700

    ok

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+int x = 0;
+while (x < 10)
+{
+	putchar(x % 10 + '0');
+	x++;
+}
+putchar('\n');
+return (0);
+
+}
