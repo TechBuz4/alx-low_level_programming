72fe86555cd79b086098f20d60b6a2473cdde77d
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-07 09:15:33 -0700

    ok

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+# include "main.h"
+/**
+ * print_last_digit - printing absolute value
+ * Return: 1 or 0
+ * @n: is char
+ */
+int print_last_digit(int n)
+{
+int j;
+j = n % 10
+if (n < 0)
+{
+	n = -n;
+}
+_putchar(n + '0');
+return (n);
+
+
+}
