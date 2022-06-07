08dcbf3d13185782cad20be037ec1384870ce054
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-07 04:52:10 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdlib.h>
+
+
+
+/**
+ *  main - Entry point
+ *   Return: Always 0 (ok)
+*/
+
+int main(void)
+{
+char name[] = "_putchar";
+int i = 0;
+while (i < 9)
+{
+	_putchar(name[i]);
+}
+	_putchar('\n');
+return (0);
+}
+
+
