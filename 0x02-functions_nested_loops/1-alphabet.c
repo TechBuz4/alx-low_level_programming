3bb6e71aa8ebc3c83f40292d2fb126b18ea162fa
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-07 05:47:09 -0700

    ok

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,11 @@
  * print_alphabet - print small letters
  *   Return: Always 0
  */
-void print_alphabet (void)
+void print_alphabet(void)
 {
 
 int i;
-for(i = 'a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 	_putchar(i);
 }
