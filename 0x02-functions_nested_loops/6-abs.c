f39d7c005e966c30c673e8ceb1a9d04ef2b3fd27
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-07 08:46:27 -0700

    ok

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _abs - printing absolute value
+ * Return: 1 or 0
+ * @n: is char
+ */
+
+
+
+int _abs(int n)
+
+{
+if (n > 0)
+{
+	_putchar('+');
+	return (n);
+}
+else if (n < 0)
+{
+	_putchar('-');
+
+	return (-n);
+}
+else
+{
+	_putchar('-');
+
+	return (n);
+}
+}
