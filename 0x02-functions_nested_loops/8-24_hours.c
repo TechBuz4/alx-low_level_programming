e7081b945f3d123f75b1af6e1042bd47de8e6884
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-07 13:28:42 -0700

    ok

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,30 +1,41 @@
 #include "main.h"
-
-
-
 /**
- * jack_bauer - tells time;
- * Description: prints time
- * Return: Always 0.
- *
+ * jack_bauer - prints time
+ *    Description: prints time
+ *      Return: Always 0.
+ */
+
 
 
 void jack_bauer(void)
 {
-	int x;
-	int y;
+
+	int x, y;
+
 	for (x = 0; x < 24; x++)
+
 	{
+
 		for (y = 0; y < 60; y++)
+
 		{
+
 			_putchar(x / 10 + 48);
+
 			_putchar(x % 10 + 48);
 
 			_putchar(':');
+
 			_putchar(y / 10 + 48);
+
 			_putchar(y % 10 + 48);
+
 			_putchar('\n');
+
 		}
+
 	}
 
 }
+
+
