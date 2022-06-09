1a505d0a33e24d557f1de7fde1fb42ba6a7fb01f
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-09 04:42:28 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+
+/**
+ * print_numbers - check if c is uppercase
+ * Return: 0
+ */
+
+
+void print_numbers(void)
+
+{
+
+	int c;
+
+	for (c = 0; c <= 9; c++)
+
+	{
+
+		_putchar(c + '0');
+
+	}
+
+	_putchar('\n');
+}
