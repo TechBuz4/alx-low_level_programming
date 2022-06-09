7a535aea6e6054b271dfe025f7bf6c44fb6677f3
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-09 10:02:51 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: 0
+*/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 101; i++)
+
+	{
+
+		if (i % 3 == 0 && i % 5 != 0)
+
+		{
+
+			printf("Fizz");
+
+		}
+
+		else if (i % 5 == 0 && i % 3 != 0)
+
+		{
+
+			printf("Buzz");
+
+		}
+
+		else if (i % 3 && i % 5 == 0)
+
+		{
+
+			printf("FizzBuzz");
+
+		}
+
+		printf("%d", i);
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
