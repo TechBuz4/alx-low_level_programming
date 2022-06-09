7c430ff01b51f6eb47915e9212b8c6217408b965
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-09 03:45:54 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+ * _isupper - check if c is uppercase
+ * Return: 1 or 0
+ * @c: is a char
+ */
+
+
+
+
+
+int _isupper(int c)
+
+{
+
+	if (c >= 'A' && c <= 'Z')
+
+	{
+
+		return (0);
+
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
+
+}
