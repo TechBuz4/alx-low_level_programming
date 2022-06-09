65f4bf5d7222b19a25786153d83deeb1d9588bc0
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-09 05:23:12 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 1 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+
+	int c, i;
+
+	while (i < 10)
+
+	{
+
+		for (c = 0; c <= 9; c++)
+
+		{
+
+			_putchar(c + '0');
+
+			_putchar('\n');
+
+		}
+
+		i++;
+
+	}
+
+	_putchar('\n');
+
+
+
+}
