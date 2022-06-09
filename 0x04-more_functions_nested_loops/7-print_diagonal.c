139daeb1cb7a45254fba4de84d29d47147fe62ab
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-09 09:06:39 -0700

    ok

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,45 +2,52 @@
 
 /**
  * print_diagonal - print lines
- *@n: is a digit
+ *@n: is an int
  */
 
+
+
 void print_diagonal(int n)
+
 {
 	if (n <= 0)
 	{
+
 		_putchar('\n');
+
 	}
 	else
 	{
+
 		int x, y;
 
 		for (x = 0; x < n; x++)
 
 		{
+
 			for (y = 0; y < n; y++)
-			{
-
-			if (x == y)
 
 			{
 
-				_putchar('\\');
+				if (x == y)
+
+				{
+
+					_putchar('\\');
+
+				}
+
+				else if (x < y)
+
+				{
+
+					_putchar(' ');
+
+				}
+
+				_putchar('\n');
 
 			}
-
-			else if (y < x)
-
-			{
-
-				_putchar(' ');
-
-			}
-
-			_putchar('\n');
-
-			}
-
+		}
 	}
-
 }
