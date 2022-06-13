454a3d29f090771e6732c8927ca2076e15982903
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-13 09:26:22 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *  puts2 -  reverse a string
+ *   @str: a string
+ *    Return: 0
+ */
+
+
+
+void puts2(char *str)
+
+{
+
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+
+	{
+
+		if (i % 2 == 0)
+
+		{
+
+			_putchar(str[i]);
+
+
+
+		}
+
+		_putchar('\n');
+
+	}
