c79065f5553bc7af8f8dfa0f2bc585037c445640
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-13 10:24:53 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ *  print_array - prints an array
+ *   @n: an int
+ *   @a: an int
+ *    Return: 0
+ */
+
+
+
+void print_array(int *a, int n)
+
+{
+
+	int i;
+
+	for (i = 0;  i <= n; i++)
+
+	{
+
+
+
+		printf("%d", *(a + i));
+
+	}
+
+	if (i != n - 1)
+
+	{
+
+		printf(", ");
+
+	}
+
+}
+
+
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ *  _strcpy - copies
+ *  @dest: an int
+ *  @src: an int
+ *   Return: 0
+ */
+
+
+
+char *_strcpy(char *dest, char *src)
+
+{
+
+	int i = 0;
+
+	while (i >= 0;
+
+	{
+
+			*(dest + i) = *(src + i);
+
+			if (*(src + i) == '\0')
+
+			{
+
+				break;
+
+			}
+
+	}
+
+			return (dest);
+
+
+}
