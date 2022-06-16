b42307402bc823a0148eb01120a100c893fe90c8
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-15 17:08:19 -0700

    ok

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include <main.h>
+
+/**
+ * _strcat - concatenating strin*
+ * @dest: a char
+ * @src: a char
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	int i = 0;
+	int j = 0;
+
+if (dest[i] != '\0')
+
+{
+
+	while (src[j] != '\0')
+
+	{
+
+		dest[i] = src[j];
+
+		j++;
+
+		i++;
+
+	}
+
+	return (dest);
+
+}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@ char *string_toupper(char *)
 char *_strncpy(char *dest, char *src, int n)
 char *_strncat(char *dest, char *src, int n)
 char *cap_string(char *)
+int _putchar(char c);
 char *leet(char *)
