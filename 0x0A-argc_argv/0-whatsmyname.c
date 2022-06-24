a15e3e7b00d5c77e1e3708b4dcd1ebe7de5a9263
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-24 04:26:16 -0700

    ok

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,10 +9,7 @@
 int main(int argc, char *argv)
 
 {
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[0]);
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 
 }
---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ * @argc: argument count
+ * @argv: argument vector
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	int cent;
+	int coins = 0;
+
+	if (argc >= 1)
+
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(args[1]);
+
+	while (cents > 0)
+
+	{
+		if (cents >= 25)
+
+		{
+			cents -= 25;
+		}
+
+		else if (cents >= 10)
+
+		{
+			cents -= 10;
+		}
+
+		else if (cents >= 5)
+
+		{
+			cents -= 5;
+		}
+
+		else if (cents >= 2)
+
+		{
+			cents -= 2;
+		}
+
+		else if (cents >= 1)
+
+		{
+			cents -= 1;
+		}
+		coins += 1;
+	}
+	printf("%d\n", coins);
+	return (0);
+
+}
+}
