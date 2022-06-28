da711b0839e21363c7949a6aa7ed4c4d3c6cc383
Busayo ososanwo <ososanwo2020@gmail.com>
2022-06-28 07:18:20 -0700

    ok

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,11 +35,11 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (i = 0; s1[i]; i++)
 	{
-		new[j] = s1[i];
+		new[j++] = s1[i];
 	}
 	for (i = 0; s2[i]; i++)
 	{
-		new[j] = s2[i];
+		new[j++] = s2[i];
 	}
 	return (new);
 }
